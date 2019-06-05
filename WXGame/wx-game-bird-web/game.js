c72fcb60819f5ba94d37f6306cafe66a5581fc43
Mrxxm <362190221@qq.com>
2019-06-05 16:44:05 +0800

    删除小游戏网页版和小程序版

--- WXGame/wx-game-bird-web/game.js
+++
@@ -1,3 +0,0 @@
-import {Main} from "./Main.js";
-
-new Main();
--- WXGame/wx-game-bird-web/js/base/Resources.js
+++
@@ -1,9 +0,0 @@
-// 图片
-export const Resources = [
-    ['land',        'res/land.png'],
-    ['birds',       'res/birds.png'],
-    ['pencilUp',    'res/pie_up.png'],
-    ['pencilDown',  'res/pie_down.png'],
-    ['background',  'res/background.png'],
-    ['startButton', 'res/start_button.png']
-];
