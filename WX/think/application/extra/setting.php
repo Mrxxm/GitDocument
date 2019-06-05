6c95ed785ef8da8054438baa218b72bf730c18d0
Mrxxm <362190221@qq.com>
2019-06-05 16:41:34 +0800

    删除tkink项目

--- WX/think/application/extra/setting.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'img_prefix' => 'http://www.think.com/images',
-    'token_expire_in' => 7200
-];
--- WX/think/runtime/cache/17/cf8b7bd761ef2ae27380ee2c6d4ce6.php
+++
@@ -1,3 +0,0 @@
-<?php
-//000000007200s:101:"{"session_key":"H60b3MYItwB74iSnbNq8dg==","openid":"oZ3xJ5EHnziqClSP5NzZJ7veM2r8","uId":1,"scope":16}";
-?>
